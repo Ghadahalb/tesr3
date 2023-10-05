4c0bcb33e3965c3b16fcda5e2a2ec27540e5ac77
GhadahAlb <ghadah.b13@gmail.com>
2023-10-05 12:06:13 +0300

    Initial Commit

---
+++ tesr3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tesr3
+//
+//  Created by Ghadah Albassam on 05/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tesr3/tesr3App.swift
@@ -0,0 +1,17 @@
+//
+//  tesr3App.swift
+//  tesr3
+//
+//  Created by Ghadah Albassam on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct tesr3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
